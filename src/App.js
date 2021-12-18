4baf050baa3809df2af200530d6d13792969012d
rafaelarissa <rafaela.larissalj@gmail.com>
2021-12-18 16:41:42 -0300

    componentizaçao

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Movie from "./components/Movie";
+import Session from "./components/Session";
+import Seat from "./components/Seat";
+import Footer from "./components/Footer";
+import Success from "./components/Success";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Movie />}></Route>
+                <Route path="/sessoes/:idFilme" element={<Movie />}></Route>
+                <Route path="/assentos/:idSessao" element={<Movie />}></Route>
+                <Route path="/" element={<Movie />}></Route>
+                <Route path="/" element={<Movie />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "./App";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
